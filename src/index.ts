38762fa6d9c9b940607cfd260937a6cfc522e97b
Arran Schlosberg <me@arranschlosberg.com>
2026-09-10 15:48:37 +0100

    chore: remove deliberate break that exercised new CI job

--- src/index.ts
+++ src/index.ts
@@ -53,8 +53,6 @@ try {
         console.info(`${version} at ${outs}`);
     };
 
-    console.info("DO NOT MERGE THIS LINE");
-
 } catch (error: any) {
     if (error instanceof Error) {
         core.setFailed(error.message)
